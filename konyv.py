dbfe40e4005193718b3d0235a37fbc8cb433f7d6
Ujzsofi00 <93594878+Ujzsofi00@users.noreply.github.com>
2023-02-07 08:29:54 +0100

    hozzaadas

--- konyv.py
+++ konyv.py
@@ -2,6 +2,13 @@ f=open("regenyek.txt","r")
 file_data=[]
 #1.
 
+for i in f:
+    if(i[-1]=='\n'):
+        file_data.append(i[:-1].split('\t'))
+    else:
+        file_data.append(i.split('\t'))
+
+del file_data[0]
 
 #2.
 
